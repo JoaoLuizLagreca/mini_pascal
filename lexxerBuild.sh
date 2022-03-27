a840e84740f0de7cd748cdfe34f1b7eed1520826
JoaoLuizLagreca <>
2022-03-27 01:21:34 -0300

    Rename build.sh to lexxerBuild.sh

---
+++ lexxerBuild.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+antlr_path="/usr/share/java"
+
+# localizar livraria
+ver=$(echo `antlr4` | awk '{ print $5 }')
+antlr_jar="$antlr_path/antlr-$ver-complete.jar"
+export CLASSPATH=".:$antlr_jar:$CLASSPATH"
+
+echo "Construindo arquivos java..."
+antlr4 src/MiniPascal.g4 -package br.senac.minipascal.compiler.lexico -o src/br/senac/minipascal/compiler/lexico
+mv src/br/senac/minipascal/compiler/lexico/src/* src/br/senac/minipascal/compiler/lexico
+rm -r src/br/senac/minipascal/compiler/lexico/src
+
+# echo "Compilando analisador lexico..."
+# javac src/br/senac/minipascal/compiler/lexico/*.java -d build/lexico/
