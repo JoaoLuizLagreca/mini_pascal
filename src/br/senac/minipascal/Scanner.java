acdc2ab4c5a373e38f471aebd0a0d6716831f8b1
JoaoLuizLagreca <>
2022-03-27 01:15:53 -0300

    Update Scanner.java

--- src/br/senac/minipascal/Scanner.java
+++ src/br/senac/minipascal/Scanner.java
@@ -9,10 +9,14 @@ public class Scanner{
 
 	public static void main(String args[]){
 		try{
+
+            if(args.length<=0)
+                throw new Exception("Uso: Scan.jar <diretório do arquivo MiniPascal>");
+
             MiniPascalLexer lexer;
             MiniPascalParser parser;
             //Ler o arquivo "Nome do arquivo" sendo entrada para o analisador lexico
-            lexer = new MiniPascalLexer(CharStreams.fromFileName("teste.pas"));
+            lexer = new MiniPascalLexer(CharStreams.fromFileName(args[0]));
 
             //cria-se um fluxo de tokens para passar para o parser
             CommonTokenStream tokenStream = new CommonTokenStream(lexer);
@@ -20,7 +24,7 @@ public class Scanner{
             //cria-se o parser a partir do tokenStream
             parser = new MiniPascalParser(tokenStream);
             parser.programa();
-            System.out.println("Copilação Efetuada com sucesso");
+            System.out.println("Compilação Efetuada com sucesso");
         }
             catch(Exception ex){
                 System.err.println("Erro "+ex.getMessage());
