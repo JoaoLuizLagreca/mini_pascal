e3709fd4549b5abc45e77eac8b13c243241e6a88
JoaoLuizLagreca <>
2022-03-27 00:57:44 -0300

    Corrigindo scripts de compilação

--- scanBuild.sh
+++ scanBuild.sh
@@ -1,11 +1,31 @@
 #!/bin/sh
+
+antlr_path="/usr/share/java"
+
 # localizar livraria
 ver=$(echo `antlr4` | awk '{ print $5 }')
-export CLASSPATH="src:/usr/share/java/antlr-$ver-complete.jar:$CLASSPATH"
+antlr_jar="$antlr_path/antlr-$ver-complete.jar"
+export CLASSPATH="src:$antlr_jar:$CLASSPATH"
 
 # localizar arquivos .java na src
 javaFiles=$(find src -name "*.java" )
 
 # Compilar
+echo "Compilando..."
 javac $javaFiles -d build && echo "Compilado com sucesso" >&0 || echo "Falha na compilação" >&2
 
+# Empacotar
+echo "Empacotando..."
+diretorios=$(ls build/)
+
+echo "Extraindo classes necessárias da livraria ANTRL4..."
+
+sucesso=0
+unzip -qo $antlr_jar javax/* org/* -d build/ && sucesso=1 || sucesso=0
+
+if [ $sucesso -eq 0 ]; then
+    echo "Erro ao extrair biblioteca" >&2
+else
+    cd build
+    jar cfm ../Scan.jar ../src/manifest.txt $diretorios && echo "Empacotado com sucesso" >&0 || echo "Falha ao empacotar" >&2
+fi
