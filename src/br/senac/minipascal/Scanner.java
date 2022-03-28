e3db6066f733ffe85949f2ee6d068656aa08a47c
JoaoLuizLagreca <>
2022-03-27 21:30:44 -0300

    Adicionado verificador de erros apropriado

--- src/br/senac/minipascal/Scanner.java
+++ src/br/senac/minipascal/Scanner.java
@@ -1,9 +1,21 @@
 package br.senac.minipascal;
 
+import java.util.BitSet;
 import br.senac.minipascal.compiler.lexico.MiniPascalLexer;
 import br.senac.minipascal.compiler.lexico.MiniPascalParser;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.ANTLRErrorListener;
+import org.antlr.v4.runtime.Parser;
+import org.antlr.v4.runtime.dfa.DFA;
+import org.antlr.v4.runtime.atn.ATNConfigSet;
+import org.antlr.v4.runtime.Recognizer;
+import org.antlr.v4.runtime.RecognitionException;
+
+import org.antlr.v4.runtime.tree.ParseTreeListener;
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.tree.ErrorNode;
+import org.antlr.v4.runtime.tree.TerminalNode;
 
 public class Scanner{
 
@@ -23,12 +35,77 @@ public class Scanner{
 
             //cria-se o parser a partir do tokenStream
             parser = new MiniPascalParser(tokenStream);
+
+            ErrorListener errorL = new ErrorListener();
+            TreeListener treeL = new TreeListener();
+
+            parser.addErrorListener(errorL);
+            parser.addParseListener(treeL);
             parser.programa();
-            System.out.println("Compilação Efetuada com sucesso");
+
+            if(!errorL.isError())
+                System.out.println("Análise léxica com sucesso!");
+
         }
             catch(Exception ex){
-                System.err.println("Erro "+ex.getMessage());
+                System.err.println("Erro do scanner "+ex.getMessage());
                 }
 	}
+
+}
+
+class ErrorListener implements ANTLRErrorListener{
+//Ouvidor de erros
+
+    private boolean erro=false;
+
+    public void  reportAmbiguity(Parser recognizer, DFA dfa, int startIndex, int stopIndex, boolean exact, BitSet ambigAlts, ATNConfigSet configs){
+
+        //System.err.println("Erro de ambiguidade: \nstartIndex: "+startIndex+"\nstopIndex: "+stopIndex+"\nexact: "+exact);
+
+    }
+
+    public void reportAttemptingFullContext(Parser recognizer, DFA dfa, int startIndex, int stopIndex, BitSet conflictingAlts, ATNConfigSet configs){
+
+         //System.err.println("Erro de ambiguidade: \nstartIndex: "+startIndex+"\nstopIndex: "+stopIndex);
+
+    }
+
+    public void reportContextSensitivity(Parser recognizer, DFA dfa, int startIndex, int stopIndex, int prediction, ATNConfigSet configs){
+
+         //System.err.println("Erro de ambiguidade: \nstartIndex: "+startIndex+"\nstopIndex: "+stopIndex+"\nprediction: "+prediction);
+
+    }
+
+    public void syntaxError(Recognizer<?,?> recognizer, Object offendingSymbol, int line, int charPositionInLine, String msg, RecognitionException e){
+
+        System.err.println("Erro de sintaxe: ");
+        System.err.println(e.getMessage());
+        e.printStackTrace();
+
+        erro=true;
+
+    }
+
+    public boolean isError(){
+        return erro;
+    }
+
+}
+
+class TreeListener implements ParseTreeListener{
+//Leitor árvore
+
+    public void visitTerminal(TerminalNode node){
+    }
+
+    public void visitErrorNode(ErrorNode node){
+    }
+
+    public void enterEveryRule(ParserRuleContext ctx){
+    }
+
+    public void exitEveryRule(ParserRuleContext ctx){
+    }
 
 }
