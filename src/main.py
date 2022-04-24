ef8cf33761cd3820cef218aa8ea493b3bc370c4e
JoaoLuizLagreca <joaoluizlagrecadasilva@gmail.com>
2022-04-24 18:15:02 -0300

    Carregar arquivo na main

---
+++ src/main.py
@@ -0,0 +1,10 @@
+import sys
+
+if len(sys.argv)!=2:
+    exit("Uso {} ARQUIVO_PASCAL".format(sys.argv[0]))
+
+
+# Abrir arquivo de programação mini pascal
+my_file = open(sys.argv[1], 'r')
+codigo=my_file.read()
+my_file.close()
