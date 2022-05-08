2c6713e8de165178a396a08ab4714e8512d7f492
JoaoLuizLagreca <joaoluizlagrecadasilva@gmail.com>
2022-05-08 19:26:55 -0300

    Script de compilação

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+descricao=mini_pascal.g4 #Localização do arquivo de descrição do ANTLR
+diretorio_antlr="src/br/senac/minipascal/antlr/" #Localização do diretório do analisador .java gerado pelo ANTLR
+
+
+#Importar biblioteca
+export CLASSPATH=".:/usr/share/java/antlr-complete.jar:$CLASSPATH"
+
+#Gerar arquivos Java
+mkdir -p $diretorio_antlr
+antlr4 $descricao -o $diretorio_antlr || exit $?
+
+#Compilar arquivos
+javaFiles=$(find src -name "*.java")
+mkdir -p build
+javac $javaFiles -d build || exit $?
