c82beca33e07034a13a45f9c191048c3bbc2606b
JoaoLuizLagreca <joaoluizlagrecadasilva@gmail.com>
2022-05-09 15:07:05 -0300

    Adicionado pascal condicional com erro

---
+++ pascal/erro/if.pas
@@ -0,0 +1,13 @@
+PROGRAM condicional;
+
+INT resp;
+
+begin
+    write('Quanto é 1+1?');
+    read(resp);
+    IF (resp == 2) THEN
+        write('Acertou mizeravi')
+    ELSE
+            write('Errou');
+            write('A resposta correta é 2')
+end.
