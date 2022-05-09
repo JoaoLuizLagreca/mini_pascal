86816e605d31dbf6c7bf9b8170c028d1013ef57b
JoaoLuizLagreca <joaoluizlagrecadasilva@gmail.com>
2022-05-09 14:43:48 -0300

    Add loop.pas

---
+++ pascal/loop.pas
@@ -0,0 +1,15 @@
+PROGRAM loop;
+
+INT l;
+BOOLEAN t;
+
+begin
+        l := 0;
+        t := TRUE;
+
+        WHILE t DO
+        BEGIN
+            writeln(l);
+            l:=l+1
+        END
+end.
