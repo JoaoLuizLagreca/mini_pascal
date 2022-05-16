845e2b45feb81bb7f377cc408aa670dce22f2aaf
JoaoLuizLagreca <joaoluizlagrecadasilva@gmail.com>
2022-05-16 20:58:40 -0300

    Pequenas mudanças no build.bat

--- build.bat
+++ build.bat
@@ -1,9 +1,18 @@
 @echo off
+
+rem Localização do java.exe versão 11 para cima
+set JAVAEXE_LOCATION=%USEPROFILE%\Documents\C:\Users\joao.llsilva1\Documents\jdk-18.0.1.1\bin\java.exe
+
 rem Localização do arquivo de descrição do ANTLR
 set descricao=mini_pascal.g4
 rem Localização do diretório do analisador .java gerado pelo ANTLR
 set diretorio_antlr="src\br\senac\minipascal\antlr\"
 
+
+
+rem Substituir comandos
+doskey java=%JAVAEXE_LOCATION% $*
+rem TODO: doskey ainda nao está armazenando localização do Java
 
 rem Importar bibliotecas
 set CLASSPATH_OLD=%CLASSPATH%
