dc758c57a407264a707e73357febe958b7c112ea
JoaoLuizLagreca <joaoluizlagrecadasilva@gmail.com>
2022-05-24 04:27:58 -0300

    Parágrafo de compilação pelo Batch

--- build.bat
+++ build.bat
@@ -3,7 +3,7 @@
 rem Configuração pré-build
 
 rem Localização do java.exe versão 11 para cima
-set JAVAEXE_LOCATION=%USERPROFILE%\Documents\jdk-18.0.1.1\bin\java.exe
+set JAVAEXE_LOCATION=%USERPROFILE%\Documents\jdk-18.0.1.1\bin\
 
 rem Localização do arquivo de descrição do ANTLR
 set descricao=mini_pascal.g4
@@ -26,10 +26,19 @@ set CLASSPATH_OLD=%CLASSPATH%
 set CLASSPATH=src;libs/antlr-complete.jar;%CLASSPATH%
 
 rem Gerar arquivos Java
-%JAVAEXE_LOCATION% org.antlr.v4.Tool %descricao% -package br.senac.minipascal.antlr -o %diretorio_antlr%
+%JAVAEXE_LOCATION%\java.exe org.antlr.v4.Tool %descricao% -package br.senac.minipascal.antlr -o %diretorio_antlr%
 if errorlevel 1 goto final
 
+rem Compilar arquivos
+Setlocal EnableDelayedExpansion
+for /F %%a in ('forfiles /S /M *.java /P src /C "cmd /c echo @path" ') do (
+set ARQ_JAVA=%%a !ARQ_JAVA!
+)
+%JAVAEXE_LOCATION%\javac.exe %ARQ_JAVA% -d build
+if errorlevel 1 goto final
 
 :final
 rem Redefinir variáveis de ambiente
 set CLASSPATH=%CLASSPATH_OLD%
+set ARQ_JAVA=
+endlocal
