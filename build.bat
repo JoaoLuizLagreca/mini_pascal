e3b9d7f1460c1882ad67fefa0060ee881ad22df8
JoaoLuizLagreca <joaoluizlagrecadasilva@gmail.com>
2022-05-24 04:57:37 -0300

    Empacotar

--- build.bat
+++ build.bat
@@ -9,6 +9,7 @@ rem Localização do arquivo de descrição do ANTLR
 set descricao=mini_pascal.g4
 rem Localização do diretório do analisador .java gerado pelo ANTLR
 set diretorio_antlr=src\br\senac\minipascal\antlr\
+set ANTLR_LIB=libs/antlr-complete.jar
 
 
 
@@ -23,7 +24,7 @@ if not exist "%JAVAEXE_LOCATION%" (
 
 rem Importar bibliotecas
 set CLASSPATH_OLD=%CLASSPATH%
-set CLASSPATH=src;libs/antlr-complete.jar;%CLASSPATH%
+set CLASSPATH=src;%ANTLR_LIB%;%CLASSPATH%
 
 rem Gerar arquivos Java
 %JAVAEXE_LOCATION%\java.exe org.antlr.v4.Tool %descricao% -package br.senac.minipascal.antlr -o %diretorio_antlr%
@@ -37,8 +38,31 @@ set ARQ_JAVA=%%a !ARQ_JAVA!
 %JAVAEXE_LOCATION%\javac.exe %ARQ_JAVA% -d build
 if errorlevel 1 goto final
 
+rem Empacotar
+mkdir temp
+7z x %ANTLR_LIB% -otemp javax/* org/* -r -y > nul
+robocopy /S /E build\ temp\ > nul
+
+for /F %%a in ('dir /Ad /b temp') do (
+set DIRETORIOS=%%a !DIRETORIOS!
+)
+cd temp
+%JAVAEXE_LOCATION%\jar.exe cfm ../MiniPascal.jar ../src/manifest.txt %DIRETORIOS%
+if errorlevel 1 goto apagar_temp
+cd ..
+rmdir /S /Q temp
+
 :final
 rem Redefinir variáveis de ambiente
 set CLASSPATH=%CLASSPATH_OLD%
 set ARQ_JAVA=
+set DIRETORIO=
 endlocal
+goto EOF
+
+:apagar_temp
+cd ..
+rmdir /S /Q temp
+goto final
+
+:EOF
