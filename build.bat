40cc103870981a3c2fd1ad305925da4a81be5d04
JoaoLuizLagreca <joaoluizlagrecadasilva@gmail.com>
2022-05-24 05:08:17 -0300

    Melhro gerenciamento de erros

--- build.bat
+++ build.bat
@@ -15,6 +15,7 @@ set ANTLR_LIB=libs/antlr-complete.jar
 
 
 
+set TEVE_ERRO=0
 
 if not exist "%JAVAEXE_LOCATION%" (
     echo Voce precisa baixar e extrair o OpenJDK 11 ou superior em um diretorio valido. Especifique a localizacao na variavel %%JAVAEXE_LOCATION%%
@@ -29,7 +30,7 @@ set CLASSPATH=src;%ANTLR_LIB%;%CLASSPATH%
 rem Gerar arquivos Java
 echo Gerando analisadores com o ANTLR4...
 %JAVAEXE_LOCATION%\java.exe org.antlr.v4.Tool %descricao% -package br.senac.minipascal.antlr -o %diretorio_antlr%
-if errorlevel 1 goto final
+if errorlevel 1 goto error
 
 rem Compilar arquivos
 echo Compilando...
@@ -38,12 +39,13 @@ for /F %%a in ('forfiles /S /M *.java /P src /C "cmd /c echo @path" ') do (
 set ARQ_JAVA=%%a !ARQ_JAVA!
 )
 %JAVAEXE_LOCATION%\javac.exe %ARQ_JAVA% -d build
-if errorlevel 1 goto final
+if errorlevel 1 goto error
 
 rem Empacotar
 echo Empacotando...
 mkdir temp
 7z x %ANTLR_LIB% -otemp javax/* org/* -r -y > nul
+if errorlevel 1 goto erro_7z
 robocopy /S /E build\ temp\ > nul
 
 for /F %%a in ('dir /Ad /b temp') do (
@@ -66,9 +68,22 @@ set DIRETORIO=
 endlocal
 goto EOF
 
-:apagar_temp
-cd ..
+:erro_7z
+echo Voce talvez nao tenha o 7zip instalado, por favor, instale
+echo Voce pode baixa-lo aqui: https://www.7-zip.org/
+set TEVE_ERRO=1
 rmdir /S /Q temp
 goto final
 
+:apagar_temp
+cd ..
+rmdir /S /Q temp
+set TEVE_ERRO=1
+goto final
+
+:error
+set TEVE_ERRO=1
+goto final
+
 :EOF
+exit /b %TEVE_ERRO%
