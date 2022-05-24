ac73945fe8ee774eff106a425b308b384d3d9ccb
JoaoLuizLagreca <joaoluizlagrecadasilva@gmail.com>
2022-05-24 03:07:42 -0300

    doskey não funciona em arquivos batch

--- build.bat
+++ build.bat
@@ -9,17 +9,12 @@ rem Localização do diretório do analisador .java gerado pelo ANTLR
 set diretorio_antlr="src\br\senac\minipascal\antlr\"
 
 
-
-rem Substituir comandos
-doskey java=%JAVAEXE_LOCATION% $*
-rem TODO: doskey ainda nao está armazenando localização do Java
-
 rem Importar bibliotecas
 set CLASSPATH_OLD=%CLASSPATH%
 set CLASSPATH=src;libs/antlr-complete.jar;%CLASSPATH%
 
 rem Gerar arquivos Java
-java org.antlr.v4.Tool %descricao% -package br.senac.minipascal.antlr -o %diretorio_antlr%
+%JAVAEXE-LOCATION% org.antlr.v4.Tool %descricao% -package br.senac.minipascal.antlr -o %diretorio_antlr%
 if errorlevel 1 goto final
 
 
