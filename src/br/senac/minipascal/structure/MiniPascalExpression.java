b617774ac0a16ac688d7feb2cf901e85487db420
JoaoLuizLagreca <joaoluizlagrecadasilva@gmail.com>
2022-06-13 16:35:50 -0300

    GETs e SETs em MiniPascalExpression

--- src/br/senac/minipascal/structure/MiniPascalExpression.java
+++ src/br/senac/minipascal/structure/MiniPascalExpression.java
@@ -9,4 +9,28 @@ public class MiniPascalExpression extends MiniPascalFactor{
         super(0);
     }
 
+    public void setFactor1(MiniPascalFactor fac1){
+        this.fac1 = fac1;
+    }
+
+    public MiniPascalFactor getFactor1(){
+        return f1;
+    }
+
+    public void setFactor2(MiniPascalFactor fac2){
+        this.fac2 = fac2;
+    }
+
+    public MiniPascalFactor getFactor2(){
+        return f2;
+    }
+
+    public void setOperator(MiniPascalOperator op){
+        this.op = op;
+    }
+
+    public MiniPascalOperator getOperator(){
+        return op;
+    }
+
 }
