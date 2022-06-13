4ae7dcfa167b6ce594fddfa9ba79c8e2fafc7409
JoaoLuizLagreca <joaoluizlagrecadasilva@gmail.com>
2022-06-13 02:13:59 -0300

    Add MiniPascalOperator.java

---
+++ src/br/senac/minipascal/structure/MiniPascalOperator.java
@@ -0,0 +1,12 @@
+package br.senac.minipascal.structure;
+
+public class MiniPascalOperator{
+
+    public static final byte ADD=0;
+    public static final byte SUB=1;
+    public static final byte MULTIPLY=2;
+    public static final byte DIVIDE=3;
+    public static final byte AND=4;
+    public static final byte OR = 5;
+
+}
