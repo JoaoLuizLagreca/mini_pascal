534829c61abb38d4ecf09749d61f722104d68777
JoaoLuizLagreca <joaoluizlagrecadasilva@gmail.com>
2022-06-13 17:31:27 -0300

    Semantic exception

---
+++ src/br/senac/minipascal/structure/MiniPascalSemanticException.java
@@ -0,0 +1,9 @@
+ package sr.senac.minipascal.structure;
+
+ public class MiniPascalSemanticException extends RuntimeException{
+
+    public MiniPascalSemanticException(String message){
+        super(message);
+    }
+
+ }
