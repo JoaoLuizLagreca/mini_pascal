dc0ce6b00146fa3ff93985a1044d5a7b3a128eb2
JoaoLuizLagreca <joaoluizlagrecadasilva@gmail.com>
2022-06-13 14:00:43 -0300

    Add multivariavel.pas

---
+++ pascal/multivariavel.pas
@@ -0,0 +1,13 @@
+PROGRAM MultiVariavel;
+
+INT a, b, c, d, e;
+
+begin
+        a := 1;
+        b := 2;
+        c:= a + b;
+        d := c + 4;
+        e := 4*(a+(c DIV d)*2);
+
+        write(a)
+end.
