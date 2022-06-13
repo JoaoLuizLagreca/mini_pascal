0d920b63cf790a528f8168b803b0257a33f80264
JoaoLuizLagreca <joaoluizlagrecadasilva@gmail.com>
2022-06-13 16:24:22 -0300

    Remova os argumentos do construtor por enquanto

--- src/br/senac/minipascal/structure/MiniPascalExpression.java
+++ src/br/senac/minipascal/structure/MiniPascalExpression.java
@@ -5,8 +5,8 @@ public class MiniPascalExpression extends MiniPascalFactor{
     private MiniPascalFactor fac1, fac2;
     private MiniPascalOperator op;
 
-    public MiniPascalExpression(byte type){
-        super(type);
+    public MiniPascalExpression(){
+        super(0);
     }
 
 }
