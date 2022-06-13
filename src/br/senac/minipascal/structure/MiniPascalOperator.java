899f23b385117a5822b9621ea156a33a7d5ba481
JoaoLuizLagreca <joaoluizlagrecadasilva@gmail.com>
2022-06-13 17:10:23 -0300

    Relacionais em MiniPascalOperator

--- src/br/senac/minipascal/structure/MiniPascalOperator.java
+++ src/br/senac/minipascal/structure/MiniPascalOperator.java
@@ -10,6 +10,12 @@ public class MiniPascalOperator{
     public static final byte DIVIDE=3;
     public static final byte AND=4;
     public static final byte OR = 5;
+    public static final byte EQUAL = 6;
+    public static final byte DIFFER = 7;
+    public static final byte LOWER = 8;
+    public static final byte LOWEREQUAL = 9;
+    public static final byte GREATEREQUAL = 10;
+    public static final byte GREATER = 11;
 
     private static Hashtable operators = new Hashtable();
     static{
@@ -19,6 +25,12 @@ public class MiniPascalOperator{
         operators.put("DIV", DIVIDE);
         operators.put("AND", AND);
         operators.put("OR", OR);
+        operators.put("=", EQUAL);
+        operators.put("<>", DIFFER);
+        operators.put("<", LOWER);
+        operators.put("<=", LOWEREQUAL);
+        operators.put(">=", GREATEREQUAL);
+        operators.put(">", GREATER);
     }
 
     private byte operator;
