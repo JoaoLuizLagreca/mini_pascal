ed7ccf5acff3f46bb3d9d4774e5d4fe775b9b21f
JoaoLuizLagreca <joaoluizlagrecadasilva@gmail.com>
2022-06-13 22:06:17 -0300

    Atribuição de Token à fator

--- src/br/senac/minipascal/structure/MiniPascalFactor.java
+++ src/br/senac/minipascal/structure/MiniPascalFactor.java
@@ -1,8 +1,11 @@
 package br.senac.minipascal.structure;
 
+import org.antlr.v4.runtime.Token;
+
 public class MiniPascalFactor{
 
     private byte type;
+    private Token token;
 
     public MiniPascalFactor(byte type){
         this.type = type;
@@ -14,6 +17,14 @@ public class MiniPascalFactor{
 
     protected void setType(byte type){
         this.type = type;
+    }
+
+    public void setToken(Token token){
+        this.token = token;
+    }
+
+    public Token getToken(){
+        return token;
     }
 
 }
